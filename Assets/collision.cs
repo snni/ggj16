097999e9f654b6192784d4ac4c0c34b77ac0bdea
spartanen <sanni.partanen@eficode.com>
2016-01-30 14:41:41 +0200

    update movement

---
+++ Assets/collision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class collision : MonoBehaviour {
+	public GameObject Player;
+	// Use this for initialization
+	void OnControllerColliderHit(ControllerColliderHit Player) {
+		Player.gameObject.transform.position = Vector3.zero;
+	}
+
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
