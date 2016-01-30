8e1618662a0cb21df11a528adc2260ca9d141609
spartanen <sanni.partanen@eficode.com>
2016-01-30 14:08:06 +0200

    add movement to molli

---
+++ Assets/movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+	public float gravity = 2;
+	public float vspeed = 1;
+	public GameObject Player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		vspeed = +0.1;
+		float h = Input.GetAxis("Horizontal");
+		float v = -(gravity * vspeed);
+
+		Player.transform.Translate(h*Time.deltaTime,v*Time.deltaTime,0);
+
+	}
+}
