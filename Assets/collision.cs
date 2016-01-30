22340db64deeaa634e5ba7dc7baf57a6affc98ef
Juuso Jalava <juuso.jalava@eficode.com>
2016-01-30 20:33:00 +0200

    add heart and tried to add hit sound to walls

--- Assets/collision.cs
+++ Assets/collision.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class collision : MonoBehaviour {
 	public int col;
-
+	public AudioSource clickAudio;
 	void OnCollisionEnter2D(Collision2D collide){
 
 		if(collide.gameObject.CompareTag("leftwall") || collide.gameObject.CompareTag("rightwall")){}
-
+		clickAudio.Play ();
 		col = -1;
 
 	}
+
+
+
 }
